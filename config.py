06b9ec249c88ff61d17c6945489e572c76f710d2
irrit-im <138384949+irrit-im@users.noreply.github.com>
2026-06-08 14:10:18 +0300

    Add image directory and joystick control settings

--- config.py
+++ config.py
@@ -10,9 +10,19 @@ CAM_WIDTH = 640
 CAM_HEIGHT = 480
 FRAME_RATE = 4
 
+# directory for storing images (relative path)
+IMAGE_DIR = "images"
+
 # bluetooth port
 BT_PORT = "/dev/rfcomm0"
 
 # lcd ports
 # sda - GPIO2
 # scl - GPIO3
+
+# Joystick controll settings
+JOYSTICK_X_CENTER = 423  # calibrate
+JOYSTICK_Y_CENTER = 419  # calibrate
+DEADZONE = 75
+STEP_SIZE = 2
+
